30b927bf55a236d9c48eb8dd2041cd64769badf8
danmtzzz <alu.22130529@correo.itlalaguna.edu.mx>
2025-02-12 13:28:32 -0600

    Practica 12 febrero terminada para TextField

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -4,48 +4,126 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
+import kotlinx.coroutines.processNextEventInCurrentThread
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GreetingPreview()
+
+
+
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = name,
-        fontSize = 100.sp,
-        lineHeight = 116.sp
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Ejemplo1Theme {
-        Greeting("Feliz Cumpleaños Jose")
-        Greeting("Android")
-        //Greeting Preveiw
+    Column{
+        //TextFieldSencillo()
+        //TextFieldKeyboard()
+        OutLineTextFieldSample()
+        TextFieldWithIcons()
+
     }
 }
+
+@Composable
+fun TextFieldSencillo(){
+    var text by remember{mutableStateOf("")}
+    Column{
+    TextField(
+        value = text,
+        onValueChange = {newText ->text = newText
+
+        },
+        label = { Text("Introduce tu nombre")},
+        placeholder = { Text("Introduce tu nombre")}
+
+    )
+        Text(text = "Tu nombre es: $text")
+
+    }
+
+}
+
+@Composable
+fun TextFieldKeyboard() {
+    var text by remember { mutableStateOf("") }
+    Column {
+        TextField(
+            value = text,
+            onValueChange = { newText ->
+                text = newText
+
+            },
+            label = { Text("Numero") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType =
+                KeyboardType.Number
+            )
+
+        )
+        Text(text = "Tu numero es: $text")
+    }
+}
+@Composable
+    fun OutLineTextFieldSample() {
+        var text by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = text,
+            label = { Text(text = "Enter Your Name") },
+            onValueChange = {
+                text = it
+            }
+        )
+        Text(text = "Tu nombre es: $text")
+    }
+
+@Composable
+    fun TextFieldWithIcons() {
+        var text by remember { mutableStateOf("") }
+         OutlinedTextField(
+            value = text,
+            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "emailIcon") },
+            //trailingIcon = { Icon(imageVector = Icons.Default.Add, contentDescription = null) },
+            onValueChange = {
+                text = it
+            },
+            label = { Text(text = "Email address") },
+            placeholder = { Text(text = "Enter your e-mail") },
+        )
+
+        Text(text = "Tu correo es: $text")
+    }
+
+
