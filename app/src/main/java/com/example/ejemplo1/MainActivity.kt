8c2c40f14629275cf219bb90e47f0ccfbe48c85c
danmtzzz <alu.22130529@correo.itlalaguna.edu.mx>
2025-02-14 13:49:49 -0600

    Practica de modifiers 14 de febrero

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -4,48 +4,104 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    GreetingPreview()
                 }
             }
         }
     }
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview(){
+    Column {
+        Content()
+        Content2()
+        Content3()
+        Content4()
+        Content5()
+    }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Content(){
+
+            Text(
+                "Daniel Martinez Carranza",
+                Modifier.padding(12.dp)
+                    .background(color = Color.Green)
+
+            )
+}
+
+@Composable
+fun Content2(){
     Text(
-        text = name,
-        fontSize = 100.sp,
-        lineHeight = 116.sp
+        "Daniel Martinez Carranza",
+        color = Color.White,
+        modifier = Modifier
+            .padding(12.dp).background(color = Color.Yellow)
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Ejemplo1Theme {
-        Greeting("Feliz Cumpleaños Jose")
-        Greeting("Android")
-        //Greeting Preveiw
-    }
+fun Content3(){
+    Text(
+        text = "Tamaño de texto",
+        color = Color.Red,
+        modifier = Modifier
+            .padding(12.dp)
+            .background(Color.Cyan)
+            .size(width = 250.dp, height = 50.dp)
+    )
 }
+
+@Composable
+fun Content4(){
+    Text(
+        text = "Texto con parent",
+        color = Color.White,
+        modifier = Modifier
+            .background(Color.Gray)
+            .padding(Dp(20f))
+            .fillMaxWidth(1f)
+    )
+}
+
+@Composable
+fun Content5(){
+    Text(
+        text = "Texto con 75% de height",
+        color = Color.Magenta,
+        modifier = Modifier
+            .background(Color.Green)
+            .fillMaxHeight(0.75f)
+    )
+}
+
+
+
+
