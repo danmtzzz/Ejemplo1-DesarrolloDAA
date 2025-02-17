92b2c97f6854508cd45fbcb19ffd6340354353ca
danmtzzz <alu.22130529@correo.itlalaguna.edu.mx>
2025-02-17 13:20:13 -0600

    Practica de modifiers 17 de febrero

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -6,16 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -38,11 +45,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview(){
     Column {
-        Content()
+       /* Content()
         Content2()
         Content3()
         Content4()
-        Content5()
+        Content5()*/
+        //AlphaModifier()
+        //RotateModifier()
+        //ScaleModifier()
+        //WeightModifier()
+        //BorderModifier()
+        ClipModifier()
     }
 }
 
@@ -99,6 +112,81 @@ fun Content5(){
         modifier = Modifier
             .background(Color.Green)
             .fillMaxHeight(0.75f)
+    )
+}
+
+@Composable
+fun AlphaModifier(){
+        Box(
+            Modifier
+                .size(250.dp)
+                .alpha(0.3f)
+                .background(Color.Red)
+        )
+}
+
+@Composable
+fun RotateModifier(){
+    Box(
+        Modifier
+            .rotate(45f)
+            .size(150.dp)
+            .background(Color.Red)
+    )
+}
+
+@Composable
+fun ScaleModifier(){
+    Box(
+        Modifier
+            .scale(scaleX = 2f, scaleY = 3f)
+            .size(200.dp, 200.dp)
+            .background(Color.Red)
+    )
+}
+
+@Composable
+fun WeightModifier(){
+    Row() {
+        Column(
+            Modifier.weight(1f).background(Color.Red)){
+            Text(text = "Weight = 1", color = Color.White)
+        }
+        Column(
+            Modifier.weight(1f).background(Color.Blue)){
+            Text(text = "Weight = 1", color = Color.White)
+        }
+        Column(
+            Modifier.weight(2f).background(Color.Green)
+        ) {
+            Text(text = "Weight = 2")
+        }
+
+    }
+}
+
+@Composable
+fun BorderModifier() {
+    Text(
+        text = "Text with Red Border",
+        modifier = Modifier
+            .padding(10.dp)
+            .background(Color.Yellow)
+            .border(2.dp,Color.Red)
+            .padding(10.dp)
+    )
+}
+
+@Composable
+fun ClipModifier() {
+    Text(
+        text = "Text with Clipped background",
+        color = Color.White,
+        modifier = Modifier
+            .padding(Dp(10f))
+            .clip(RoundedCornerShape(25.dp))
+            .background(Color.Blue)
+            .padding(Dp(15f))
     )
 }
 
